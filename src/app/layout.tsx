f9e46cefe8ba1e9c09cf0877a6fbd31e34c872c9
有才 <865239789@qq.com>
2024-12-09 16:56:25 +0800

    add google count

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,13 @@ import { AwardsProvider } from '@/contexts/AwardsContext'
 import LanguageSwitch from '@/components/LanguageSwitch'
 import './globals.css'
 import { Metadata } from 'next'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
 
 export const metadata: Metadata = {
   title: '科学奖项集锦',
   description: '探索世界各地的科学奖项，了解科学界的杰出成就',
   icons: {
-    icon: '/logoipsum-268.svg',    // 使用 public 目录下的 SVG
-    // 或者直接使用 app 目录下的 SVG
-    // icon: '/logo268.svg',       
+    icon: '/logoipsum-268.svg',      
   }
 }
 
@@ -26,6 +25,7 @@ export default function RootLayout({
           <AwardsProvider>
             <LanguageSwitch />
             {children}
+            <GoogleAnalytics />
           </AwardsProvider>
         </LanguageProvider>
       </body>
---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import Script from 'next/script'
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-X8F9YK44C0"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-X8F9YK44C0');
+        `}
+      </Script>
+    </>
+  )
+} 
